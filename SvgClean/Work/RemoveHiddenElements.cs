f8f4ef4ea2e7f0fde164c7cf0ff68b25dc68fd7e
dabutvin <butvinik@outlook.com>
2019-02-21 08:54:48 -0800

    add more selectors to RemoveHiddenElements

--- SvgClean/Work/RemoveHiddenElements.cs
+++ SvgClean/Work/RemoveHiddenElements.cs
@@ -9,18 +9,29 @@ using SvgClean.Format;
 namespace SvgClean.Work
 {
     // display: none element and all the children can be removed
+    // <circle> with r <= 0 "A value lower or equal to zero disables rendering of the circle."
     internal class RemoveHiddenElements : IWork
     {
         public string Work(string input)
         {
             var parser = new HtmlParser();
             var document = parser.ParseDocument(input).GetElementsByTagName("svg")[0];
+            var selectors = new[]
+            {
+                "[display='none']",
+                "[visibility='hidden']",
+                "[width='0']",
+                "[height='0']",
+                "[r='0']",
+            };
 
-            var element = document.QuerySelectorAll("[display='none']").FirstOrDefault();
+            var selector = string.Join(",", selectors);
+
+            var element = document.QuerySelector(selector);
             while (element != null)
             {
                 element.Remove();
-                element = document.QuerySelectorAll("[display='none']").FirstOrDefault();
+                element = document.QuerySelector(selector);
             }
 
             return document.ToSvgString();
--- Test/WorkTests/RemoveHiddenElementsTest.cs
+++ Test/WorkTests/RemoveHiddenElementsTest.cs
@@ -18,6 +18,11 @@ namespace Test.WorkTests
                 { "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><path display=\"none\" d=\"M5.05.01c.81 2.17.41\"/></svg>", "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"></svg>" },
                 { "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><g display=\"none\"><path d=\"M5.05.01c.81 2.17.41\"/><path d=\"M5.05.01c.81 2.17.41\"/></g></svg>", "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"></svg>" },
                 { "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><path display=\"none\" d=\"M5.05.01c.81 2.17.41\"/><path display=\"none\" d=\"M5.05.01c.81 2.17.41\"/></svg>", "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"></svg>" },
+                { "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><path display=\"none\" d=\"M5.05.01c.81 2.17.41\"/><path visibility=\"hidden\" d=\"M5.05.01c.81 2.17.41\"/></svg>", "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"></svg>" },
+                { "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><circle r=\"0\" /><path d=\"M5.05.01c.81 2.17.41\"/></svg>", "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><path d=\"M5.05.01c.81 2.17.41\"/></svg>" },
+                { "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><rect width=\"0\" /><path d=\"M5.05.01c.81 2.17.41\"/></svg>", "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><path d=\"M5.05.01c.81 2.17.41\"/></svg>" },
+                { "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><rect height=\"0\" /><path d=\"M5.05.01c.81 2.17.41\"/></svg>", "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><path d=\"M5.05.01c.81 2.17.41\"/></svg>" },
+                { "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><rect height=\"0\" /><circle r=\"0\" /><path d=\"M5.05.01c.81 2.17.41\"/></svg>", "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 12 16\"><path d=\"M5.05.01c.81 2.17.41\"/></svg>" },
             };
             foreach (var input in data.Keys)
             {
